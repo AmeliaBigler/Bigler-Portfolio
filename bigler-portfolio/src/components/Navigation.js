faab569b7b76fce45f3812735ed149480dc0d869
Amelia Bigler <amaebigler@gmail.com>
2023-05-30 15:57:39 -0500

    add google font. add contact me content. remove resume tab.

--- bigler-portfolio/src/components/Navigation.js
+++ bigler-portfolio/src/components/Navigation.js
@@ -33,15 +33,6 @@ function NavTabs({ currentPage, handlePageChange }) {
           Contact
         </a>
       </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
     </ul>
   );
 }
